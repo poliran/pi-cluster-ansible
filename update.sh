e1b26fe60f220fb2ffb3b44e83fb525a1f759303
poliran <keith.poliran@gmail.com>
2025-09-25 16:40:01 +0800

    Clean up project: remove unnecessary files and directories

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "=== Quick Application Update ==="
-
-# Only update web application
-ansible-playbook -i inventories/production/hosts playbooks/site.yml --tags "webapp"
-
-echo "=== Update completed ==="
